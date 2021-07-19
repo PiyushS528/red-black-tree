30a75c7e722afa8657b889b012d7324554b3ef59
PiyushS528 <62088642+PiyushS528@users.noreply.github.com>
2021-07-19 17:59:21 +0530

    Create rb-tree-script.js
    Created on 2021-Jul-19, 05:58 PM IST

---
+++ rb-tree-script.js
@@ -0,0 +1,40 @@
+var c;
+
+function Node (val, isred) {        // new node factory
+    var value = val || 0;
+    var red = isred || false;       // if 'red' is true => node is red in color, otherwise it is black
+    
+    var getv = function getv() { return value; }        // get value
+    var getc = function getc() { return red; }      // get color
+    var togglec = function togglec () {     // toggle between red and black
+        red = (red === true) ? false : true;
+    }
+    
+    return {
+        getv: getv,
+        getc: getc,
+        togglec: togglec
+    };
+}
+
+function redraw (val, operation) {
+    if (operation === 1) {      // Insert Operation
+        var n = Node(val);
+}
+
+function updateTree() {
+    var operation;
+    if (document.getElementById("insert").checked)
+        operation = 1;
+    if (document.getElementById("delete").checked)
+        operation = 2;
+    if (document.getElementById("search").checked)
+        operation = 3;
+    
+    redraw(document.getElementById("nodeVal").value, operation);
+}
+
+(function initCanvas() {
+    var canv = document.getElementById("canvasArea");
+    c = canv.getContext("2d");
+})();
