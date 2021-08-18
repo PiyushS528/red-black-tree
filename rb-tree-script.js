c8f060725f950d0333a90185b930638726468cfe
PiyushS528 <62088642+PiyushS528@users.noreply.github.com>
2021-08-18 08:28:20 +0530

    Update rb-tree-script.js
    - Deletion process is finally working in its full form, without many cases tested.
    - A bug was fixed wherein a child (right child) of a successor node (in deletion) moved a great length towards the right during substitution animation. That happened because the coordinates were miscalculated for dangling node during tree offset for centering.
    - Rotation buttons are removed from the page as they're not needed anymore (they were there since early on in the project, mostly for testing purposes).
    - Animations for deletion operation are better.

--- rb-tree-script.js
+++ rb-tree-script.js
@@ -1,11 +1,3 @@
-/* **** TO DO ****
-
-- SEARCH FUNCTION WITH ANIMATION
-- IMPLEMENT INSERTION AND DELETION FUNCTIONS WITHOUT GENERATORS
-- BETTER WAY TO IMPLEMENT CHANGES[] ARRAY FOR ANIMATION
-
- **************** */
-
 
 /* ***************************
 Dependencies from treeObj.js:
@@ -51,14 +43,15 @@ var canvasProp = {	// Dimensions of canvas
 var treeProp = {		// properties of tree as drawn in canvas
 	marginX: 60,
 	marginY: 60,
-	ySpacing: 80,
-	nodeRadius: 24,
+	xSpacing: 50,
+	ySpacing: 100,
+	nodeRadius: 35,
 
 	red: "rgb(180, 16, 16)",
 	black: "#000000",
 	stroke: "rgb(172, 224, 114)",
 
-	font: "28px arial narrow",
+	font: "40px arial narrow",
 	fColor: "#ffffff"
 };
 var transition = {
@@ -66,12 +59,14 @@ var transition = {
 	state: [], 		//[[Node(), Node(), ...], [...], ...];
 	steps: [], 		//[[{node: 10, from: 21}, {..., ...}], [{...}, {...}, ...]],
 	other: [],
+	tempState: [],
 
 	reset: function reset() {
 		this.animtype = [];
 		this.state = [];
 		this.steps = [];
 		this.other = [];
+		this.tempState = [];
 	}
 };
 
@@ -145,17 +140,21 @@ function drawLine (x1, y1, x2, y2) {
 	c.closePath();
 }
 
-function drawTree (nodes, other) {
+function drawTree (nodes, other, skipnodes) {
 	if (other === undefined) other = [];
+	if (skipnodes === undefined) skipnodes = [];
 
 	if (!nodes) nodes = tree.nodes;
 	clearCanvas();
 
-	var cIndex = 0;
+	var skipIndex = 0;
 
 	for (var i = 0; i < nodes.length; ++i) {
 		if (nodes[i]) {
-			drawNode (nodes[i].value, nodes[i].x, nodes[i].y, (nodes[i].color !== -1) ? nodes[i].cvalue : -1);
+			if (skipnodes && i === skipnodes[skipIndex]) {
+				++skipIndex;
+			}
+			else drawNode (nodes[i].value, nodes[i].x, nodes[i].y, (nodes[i].color !== -1) ? nodes[i].cvalue : -1);
 
 			if (i !== 0 && nodes[((i-1)/2)|0])
 				drawLine(nodes[i].x, nodes[i].y, nodes[((i-1)/2)|0].x, nodes[((i-1)/2)|0].y);
@@ -168,87 +167,153 @@ function drawTree (nodes, other) {
 	}
 }
 
-function recordChanges (nodes, animtype, changes, init) {  // [animtype: 0 - move, 1 - add, 2 - remove, 3 - recolor, 4 - search, 5 - successor search, 6 - search blink, 7 - successor search blink]
-	if (init === undefined) init = false;
-	var treeWidth = canvasProp.width - 2 * treeProp.marginX;
+function calcNodePositions (nodes) {
+	if (nodes === undefined) nodes = tree.nodes;
+	if (!nodes[0]) return;
 
-	function calcPos (ind) {
-		l1 = tree.levelOf(ind, true) / 2;
-		l2 = l1 - 1;
+	var q = [];
+	var curr = 0;
+	var xcurr = treeProp.marginX;
 
-		return {
-			x: treeProp.marginX + (ind - l2 + 0.5) * treeWidth / l1,
-			y: treeProp.marginY + treeProp.ySpacing * tree.levelOf(ind)
-		};
+	while (q.length !== 0 || curr === 0) {
+		if (nodes[curr]) {
+			q.push (curr);
+			curr = curr * 2 + 1;
+		}
+		else {
+			curr = q.pop();
+			nodes[curr].x = xcurr;
+			xcurr += treeProp.xSpacing;
+			nodes[curr].y = treeProp.marginY + treeProp.ySpacing * tree.levelOf(curr);
+
+			curr = curr * 2 + 2;
+			if (nodes[curr]) {
+				q.push (curr);
+				curr = curr * 2 + 1;
+			}
+		}
 	}
+
+	curr = ((curr - 1) / 2) | 0;
+
+	var lastOffset = (nodes[curr].x) - (canvasProp.width - treeProp.marginX);
+	var newOffsetx;
+
+	if (lastOffset <= 0) {		// Offset all nodes by certain amount
+		newOffsetx = (-lastOffset / 2) | 0;
+		for (var i = 0; i < nodes.length; ++i)
+			if (nodes[i] && (nodes[((i - 1) / 2) | 0] || i === 0)) nodes[i].x += newOffsetx;
+		newOffsetx = 1;
+	}
+	else {		// Squeeze to fit
+		newOffsetx = (canvasProp.width - 2 * treeProp.marginX) / (canvasProp.width - 2 * treeProp.marginX + lastOffset);
+		for (var i = 0; i < nodes.length; ++i) {
+			if (nodes[i] && (nodes[((i - 1) / 2) | 0] || i === 0)) {
+				nodes[i].x -= treeProp.marginX;
+				nodes[i].x = (nodes[i].x * newOffsetx) | 0;
+				nodes[i].x += treeProp.marginX;
+			}
+		}
+	}
+	return treeProp.xSpacing * newOffsetx;
+}
+
+function recordChanges (nodes, animtype, changes, init) {
+	/* animtypes:
+		0 - move
+		1 - add node
+		2 - remove node
+		3 - recolor
+		4 - search
+		5 - successor search
+		6 - search blink
+		7 - successor search blink
+		8 - move and toggle color
+		9 - blink nodes (for color changing to/from double black)
+	*/
+
+	if (init === undefined) init = false;
 	if (init) {
 		transition.reset();
-		for (var i = 0; i < tree.nodes.length; ++i) {
-			if (!tree.nodeExists(i)) continue;
-
-			var p = calcPos(i);
-			tree.nodes[i].x = p.x;
-			tree.nodes[i].y = p.y;
-		}
+		calcNodePositions (tree.nodes);
 		transition.state[0] = nodeCpy(tree.nodes);
 		return;
 	}
 
-	var curr = transition.steps.length;
+	var nodeSpacingX;
 
 	changes.sort(function(a, b) {return a.node - b.node});
 
-	if (animtype === 0) {
-		for (var i = 0; i < changes.length; ++i) {
-			if (!tree.nodeExists(changes[i].node)) continue;
-			var p = calcPos (changes[i].from);
-			changes[i].x1 = p.x;
-			changes[i].y1 = p.y;
+	if (animtype !== 2) nodeSpacingX = calcNodePositions (nodes);
 
-			p = calcPos (changes[i].node);
-			nodes[changes[i].node].x = changes[i].x2 = p.x;
-			nodes[changes[i].node].y = changes[i].y2 = p.y;
+	if (animtype === 0 || animtype === 8) {		// move / move and toggle color
+		for (var i = 0; i < changes.length; ++i) {
+			if (!nodes[changes[i].node]) continue;
+			changes[i].x1 = transition.state[transition.state.length - 1][changes[i].from].x;
+			changes[i].y1 = transition.state[transition.state.length - 1][changes[i].from].y;
+
+			changes[i].x2 = nodes[changes[i].node].x;
+			changes[i].y2 = nodes[changes[i].node].y;
+
+			if (animtype === 8)
+				nodes[changes[i].node].cvalue = (changes[i].color === 0) ? treeProp.black : treeProp.red;
 		}
 	}
-	else if (animtype === 1) {
+
+	else if (animtype === 1) {		// add node
 		for (var i = 0; i < changes.length; ++i) {
 			changes[i].x1 = canvasProp.width / 2;
 			changes[i].y1 = canvasProp.height + treeProp.nodeRadius + 2;
 
-			var p = calcPos (changes[i].node);
-			nodes[changes[i].node].x = changes[i].x2 = p.x;
-			nodes[changes[i].node].y = changes[i].y2 = p.y;
+			changes[i].x2 = nodes[changes[i].node].x;
+			changes[i].y2 = nodes[changes[i].node].y;
 			nodes[changes[i].node].cvalue = (nodes[changes[i].node].color === 0) ? treeProp.black : treeProp.red;
 		}
 	}
-	else if (animtype === 2) {
+	else if (animtype === 2) {		// remove node
 		for (var i = 0; i < changes.length; ++i) {
-			var p = calcPos (changes[i].node);
-			changes[i].x1 = p.x;
-			changes[i].y1 = p.y;
+			changes[i].x1 = transition.state[transition.state.length - 1][changes[i].node].x;
+			changes[i].y1 = transition.state[transition.state.length - 1][changes[i].node].y;
 
 			changes[i].x2 = canvasProp.width / 2;
 			changes[i].y2 = canvasProp.height + treeProp.nodeRadius + 2;
 		}
 	}
-	else if (animtype === 3) {
+	else if (animtype === 3) {		// recolor
 		for (var i = 0; i < changes.length; ++i) {
 			nodes[changes[i].node].cvalue = (changes[i].color === 0) ? treeProp.black : treeProp.red;
 		}
 	}
-	else if (animtype === 4 || animtype === 5) {
-		var p = calcPos (changes[0].from);
-		changes[0].x1 = p.x;
-		changes[0].y1 = p.y;
+	else if (animtype === 4 || animtype === 5) {		// search
+		changes[0].x1 = nodes[changes[0].from].x;
+		changes[0].y1 = nodes[changes[0].from].y;
 
-		p = calcPos (changes[0].node);
-		changes[0].x2 = p.x;
-		changes[0].y2 = p.y;
+
+		if (!nodes[changes[0].node]) {	// in case it's a new node to be inserted, the circle will move to the empty spot where the new node is to be inserted
+			changes[0].y2 = changes[0].y1 + treeProp.ySpacing;
+			if (changes[0].node % 2 === 1) changes[0].x2 = changes[0].x1 - nodeSpacingX;
+			else  changes[0].x2 = changes[0].x1 + nodeSpacingX;
+		}
+		else {
+			changes[0].x2 = nodes[changes[0].node].x;
+			changes[0].y2 = nodes[changes[0].node].y;
+		}
 	}
-	else if (animtype === 6 || animtype === 7) {
-		var p = calcPos (changes[0].node);
-		changes[0].x = p.x;
-		changes[0].y = p.y;
+	else if (animtype === 6 || animtype === 7) {		// search blink
+		if (!nodes[0]) {
+			changes[0].x = canvasProp.width / 2;
+			changes[0].y = treeProp.marginY;
+		}
+		else if (nodes[changes[0].node]) {
+			changes[0].x = nodes[changes[0].node].x;
+			changes[0].y = nodes[changes[0].node].y;
+		}
+		else {		// in case it's a new node to be inserted, the circle will blink at the empty spot where the new node is to be inserted
+			changes[0].x = transition.steps[transition.steps.length - 1][0].x2;
+			changes[0].y = transition.steps[transition.steps.length - 1][0].y2;
+		}
+	}
+	else if (animtype === 9) {		// blink nodes
 	}
 
 	transition.animtype.push (animtype);
@@ -259,50 +324,63 @@ function recordChanges (nodes, animtype, changes, init) {  // [animtype: 0 - mov
 function animate (args) {
 	if (args === undefined) args = {};
 	if (animFrame) return;
+	if (args.stepNumber === undefined) args.stepNumber = 0;
 
-	if (args.nOperation === undefined) args.nOperation = 0;
-
-	var nodes = [], othernodes = [];
-
-	if (args.nOperation >= transition.steps.length) {
-		drawTree(transition.state[args.nOperation + 1]);
+	if (args.stepNumber >= transition.steps.length) {
+		calcNodePositions (transition.state[args.stepNumber + 1]);
+		calcNodePositions (tree.nodes);
+		drawTree(tree.nodes);
 		return;
 	}
 
-	var k = 0, updatenodes;
+	var nodes = [], othernodes = [], skipnodes = [], tSkipnodes = [];
+	var updatenodes = function (){}, postAnimation = function (){};
 
-	if (transition.animtype[args.nOperation] === 0 || transition.animtype[args.nOperation] === 1) {
-		nodes = nodeCpy(transition.state[args.nOperation + 1]);
+	if (transition.animtype[args.stepNumber] === 0 || transition.animtype[args.stepNumber] === 1) { // move / add node
+		nodes = nodeCpy(transition.state[args.stepNumber + 1]);
+		transition.tempState = nodeCpy (nodes);
+
 		updatenodes = function updatenodes (p) {
-			for (var i = 0; i < transition.steps[args.nOperation].length; ++i) {
-				var ci = transition.steps[args.nOperation][i];
-				if (nodes[ci.node] === null) continue;			// To be removed later
-
-				nodes[ci.node].x = ci.x1 + (ci.x2 - ci.x1) * p;
-				nodes[ci.node].y = ci.y1 + (ci.y2 - ci.y1) * p;
+			var highlightedNode = 0;
+			var currstep = transition.steps[args.stepNumber];
+			for (var i = 0; i < nodes.length; ++i) {
+				if (!nodes[i]) continue;
+				if (highlightedNode < currstep.length && i === currstep[highlightedNode].node) {
+					var ci = currstep[highlightedNode];
+					nodes[i].x = ci.x1 + (ci.x2 - ci.x1) * p;
+					nodes[i].y = ci.y1 + (ci.y2 - ci.y1) * p;
+					++highlightedNode;
+					continue;
+				}
+				nodes[i].x = transition.state[args.stepNumber][i].x + (transition.tempState[i].x - transition.state[args.stepNumber][i].x) * p;
+				nodes[i].y = transition.state[args.stepNumber][i].y + (transition.tempState[i].y - transition.state[args.stepNumber][i].y) * p;
 			}
 		}
+		postAnimation = function postAnimation () {
+			transition.tempState = [];
+		}
 	}
-	else if (transition.animtype[args.nOperation] === 2) {
-		nodes = nodeCpy(transition.state[args.nOperation + 1]);
-		for (var i = 0; i < transition.steps[args.nOperation].length; ++i) {
-			othernodes[i] = nodeCpy([transition.state[args.nOperation][transition.steps[args.nOperation][i].node]])[0];
+	else if (transition.animtype[args.stepNumber] === 2) {		// remove
+		nodes = nodeCpy(transition.state[args.stepNumber]);
+		for (var i = 0; i < transition.steps[args.stepNumber].length; ++i) {
+			skipnodes[i] = transition.steps[args.stepNumber][i].node;
+			othernodes[i] = nodeCpy([nodes[skipnodes[i]]])[0];
 		}
 		updatenodes = function updatenodes(p) {
-			for (i = 0; i < transition.steps[args.nOperation].length; ++i) {
-				var ci = transition.steps[args.nOperation][i];
+			for (i = 0; i < transition.steps[args.stepNumber].length; ++i) {
+				var ci = transition.steps[args.stepNumber][i];
 
 				othernodes[i].x = ci.x1 + (ci.x2 - ci.x1) * p;
 				othernodes[i].y = ci.y1 + (ci.y2 - ci.y1) * p;
 			}
 		}
 	}
-	else if (transition.animtype[args.nOperation] === 3) {
-		nodes = nodeCpy(transition.state[args.nOperation + 1]);
+	else if (transition.animtype[args.stepNumber] === 3) {		// recolor
+		nodes = nodeCpy(transition.state[args.stepNumber + 1]);
 		updatenodes = function updatenodes(p) {
-			for (var i = 0; i < transition.steps[args.nOperation].length; ++i) {
+			for (var i = 0; i < transition.steps[args.stepNumber].length; ++i) {
 				var r, g, b;
-				if (transition.steps[args.nOperation][i].color === 0) {
+				if (transition.steps[args.stepNumber][i].color === 0) {
 					r = (treeProp.r1 + (treeProp.r0 - treeProp.r1) * p);
 					g = (treeProp.g1 + (treeProp.g0 - treeProp.g1) * p);
 					b = (treeProp.b1 + (treeProp.b0 - treeProp.b1) * p);
@@ -312,55 +390,115 @@ function animate (args) {
 					g = (treeProp.g0 + (treeProp.g1 - treeProp.g0) * p);
 					b = (treeProp.b0 + (treeProp.b1 - treeProp.b0) * p);
 				}
-				nodes[transition.steps[args.nOperation][i].node].cvalue = 'rgb(' + r + ',' + g + ',' + b + ')';
+				nodes[transition.steps[args.stepNumber][i].node].cvalue = 'rgb(' + (r|0) + ',' + (g|0) + ',' + (b|0) + ')';
 			}
 		}
 	}
-	else if (transition.animtype[args.nOperation] === 4) {
-		nodes = nodeCpy(transition.state[args.nOperation + 1]);
+	else if (transition.animtype[args.stepNumber] === 4) {		// search
+		nodes = nodeCpy(transition.state[args.stepNumber + 1]);
 		othernodes[0] = new Node(null, -2);
 		updatenodes = function updatenodes (p) {
-			var ci = transition.steps[args.nOperation][0];
+			var ci = transition.steps[args.stepNumber][0];
 			othernodes[0].x = ci.x1 + (ci.x2 - ci.x1) * p;
 			othernodes[0].y = ci.y1 + (ci.y2 - ci.y1) * p;
 		}
 	}
-	else if (transition.animtype[args.nOperation] === 5) {
-		nodes = nodeCpy(transition.state[args.nOperation + 1]);
+	else if (transition.animtype[args.stepNumber] === 5) {		// successor search
+		nodes = nodeCpy(transition.state[args.stepNumber + 1]);
 		othernodes[0] = new Node(null, -3);
 		updatenodes = function updatenodes (p) {
-			var ci = transition.steps[args.nOperation][0];
+			var ci = transition.steps[args.stepNumber][0];
 			othernodes[0].x = ci.x1 + (ci.x2 - ci.x1) * p;
 			othernodes[0].y = ci.y1 + (ci.y2 - ci.y1) * p;
 		}
 	}
-	else if (transition.animtype[args.nOperation] === 6) {
-		nodes = nodeCpy(transition.state[args.nOperation + 1]);
+	else if (transition.animtype[args.stepNumber] === 6) {		// search blink
+		nodes = nodeCpy(transition.state[args.stepNumber + 1]);
 		updatenodes = function updatenodes (p) {
-			if ((p * 10) % 2 > 1) othernodes = [];
+			if ((p * 8) % 2 > 1) othernodes = [];
 			else {
-				var ci = transition.steps[args.nOperation][0];
+				var ci = transition.steps[args.stepNumber][0];
 				othernodes[0] = new Node(null, -2);
 				othernodes[0].x = ci.x;
 				othernodes[0].y = ci.y;
 			}
 		}
 	}
-	else if (transition.animtype[args.nOperation] === 7) {
-		nodes = nodeCpy(transition.state[args.nOperation + 1]);
+	else if (transition.animtype[args.stepNumber] === 7) {		// successor search blink
+		nodes = nodeCpy(transition.state[args.stepNumber + 1]);
 		updatenodes = function updatenodes (p) {
-			if ((p * 10) % 2 > 1) othernodes = [];
+			if ((p * 8) % 2 > 1) othernodes = [];
 			else {
-				var ci = transition.steps[args.nOperation][0];
+				var ci = transition.steps[args.stepNumber][0];
 				othernodes[0] = new Node(null, -3);
 				othernodes[0].x = ci.x;
 				othernodes[0].y = ci.y;
 			}
 		}
 	}
+	else if (transition.animtype[args.stepNumber] === 8) {		// move and toggle
+		nodes = nodeCpy(transition.state[args.stepNumber + 1]);
+		transition.tempState = nodeCpy (nodes);
+
+		updatenodes = function updatenodes (p) {
+			var highlightedNode = 0;
+			var currstep = transition.steps[args.stepNumber];
+			for (var i = 0; i < nodes.length; ++i) {
+				if (nodes[i] === null || nodes[i] === undefined) continue;
+				if (highlightedNode < currstep.length && i === currstep[highlightedNode].node) {
+					var ci = currstep[highlightedNode];
+					nodes[i].x = ci.x1 + (ci.x2 - ci.x1) * p;
+					nodes[i].y = ci.y1 + (ci.y2 - ci.y1) * p;
+
+					var r, g, b;
+					if (ci.color === 0) {
+						r = (treeProp.r1 + (treeProp.r0 - treeProp.r1) * p);
+						g = (treeProp.g1 + (treeProp.g0 - treeProp.g1) * p);
+						b = (treeProp.b1 + (treeProp.b0 - treeProp.b1) * p);
+					}
+					else {
+						r = (treeProp.r0 + (treeProp.r1 - treeProp.r0) * p);
+						g = (treeProp.g0 + (treeProp.g1 - treeProp.g0) * p);
+						b = (treeProp.b0 + (treeProp.b1 - treeProp.b0) * p);
+					}
+					nodes[ci.node].cvalue = 'rgb(' + (r|0) + ',' + (g|0) + ',' + (b|0) + ')';
+
+					++highlightedNode;
+				}
+				else {
+					nodes[i].x = transition.state[args.stepNumber][i].x + (transition.tempState[i].x - transition.state[args.stepNumber][i].x) * p;
+					nodes[i].y = transition.state[args.stepNumber][i].y + (transition.tempState[i].y - transition.state[args.stepNumber][i].y) * p;
+				}
+			}
+		}
+		postAnimation = function postAnimation () {
+			transition.tempState = [];
+		}
+	}
+	else if (transition.animtype[args.stepNumber] === 9) {		// blink node
+		nodes = nodeCpy(transition.state[args.stepNumber + 1]);
+		transition.tempState = nodeCpy (nodes);
+
+		for (var i = 0; i < transition.steps[args.stepNumber].length; ++i) {
+			skipnodes.push(transition.steps[args.stepNumber][i].node);
+		}
+		tSkipnodes = skipnodes.slice();
+		updatenodes = function updatenodes (p) {
+			if ((p * 8) % 2 > 1) skipnodes = [];
+			else skipnodes = tSkipnodes.slice();
+			for (var i = 0; i < nodes.length; ++i) {
+				if (!transition.state[args.stepNumber][i]) continue;
+				nodes[i].x = transition.state[args.stepNumber][i].x + (transition.tempState[i].x - transition.state[args.stepNumber][i].x) * p;
+				nodes[i].y = transition.state[args.stepNumber][i].y + (transition.tempState[i].y - transition.state[args.stepNumber][i].y) * p;
+			}
+		}
+		postAnimation = function postAnimation () {
+			transition.tempState = [];
+		}
+	}
 
 	updatenodes (0);
-	drawTree (nodes, othernodes);
+	drawTree (nodes, othernodes, skipnodes);
 
 	var totalIntervals = progress.length;
 	var animStart, animStop;
@@ -373,14 +511,16 @@ function animate (args) {
 		if (t >= animStart) {
 			var prog = progress[(totalIntervals * (t - animStart) / (animStop - animStart)) | 0];
 			updatenodes (prog);
-			drawTree (nodes, othernodes);
+			drawTree (nodes, othernodes, skipnodes);
 		}
 
 		if (t >= animStop) {
 			cancelAnimationFrame (animFrame);
 			animFrame = null;
 
-			animate({nOperation: args.nOperation + 1});
+			postAnimation();
+
+			animate({stepNumber: args.stepNumber + 1});
 		}
 		else {
 			animFrame = requestAnimationFrame(mainAnimation);
@@ -514,4 +654,6 @@ window.onload = function initCanvas() {
 	}
 
 	parseColors();
+
+	tree.setRecorder (recordChanges);
 }
